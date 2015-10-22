1d7da5433d1c36203fc0b93243c8be8d2c87591d
Fauve <contact@taniere.info>
2015-10-22 16:15:13 +0100

    Modifing hex2bibi() and many other improvings.

--- bibi.php
+++ bibi.php
@@ -6,7 +6,7 @@
  * This file is part of the Deximalisation Project.
  *
  * @package   bibi
- * @author    Fauve <Fauve DOT ordinator AT taniere DOT info>
+ * @author    Fauve <contact@taniere.info>
  * @copyright © 󰀇󰀍󰀎 ↔ 2015 — Fauve, Deximalisation Project.
  * @license   MIT http://opensource.org/licenses/mit-license.php
  *
@@ -19,48 +19,71 @@ $decimal2bibinaryCorrespondence = [
     "DO", "DA", "DE", "DI"
 ];
 
-class constructNewCharacter
-{
-	public $litteral;
-	public $decimal;
-	public $character;
+//class constructNewCharacter
+//{
+	//public $litteral;
+	//public $decimal;
+	//public $character;
 
-	function __construct ($litteral, $decimal, $character)
-	{
-		$this->litteral  = $litteral;
-		$this->decimal   = $decimal;
-		$this->character = $character;
-	}
-}
+	//function __construct ($litteral, $decimal, $character)
+	//{
+		//$this->litteral  = $litteral;
+		//$this->decimal   = $decimal;
+		//$this->character = $character;
+	//}
+//}
+
+//$litt2dec2bibi = [
+	//new constructNewCharacter("HO",  0, "󰀀"),
+	//new constructNewCharacter("HA",  1, "󰀁"),
+	//new constructNewCharacter("HE",  2, "󰀂"),
+	//new constructNewCharacter("HI",  3, "󰀃"),
+	//new constructNewCharacter("BO",  4, "󰀄"),
+	//new constructNewCharacter("BA",  5, "󰀅"),
+	//new constructNewCharacter("BE",  6, "󰀆"),
+	//new constructNewCharacter("BI",  7, "󰀇"),
+	//new constructNewCharacter("KO",  8, "󰀈"),
+	//new constructNewCharacter("KA",  9, "󰀉"),
+	//new constructNewCharacter("KE", 10, "󰀊"),
+	//new constructNewCharacter("KI", 11, "󰀋"),
+	//new constructNewCharacter("DO", 12, "󰀌"),
+	//new constructNewCharacter("DA", 13, "󰀍"),
+	//new constructNewCharacter("DE", 14, "󰀎"),
+	//new constructNewCharacter("DI", 15, "󰀏"),
+//];
 
 $litt2dec2bibi = [
-	new constructNewCharacter("HO",  0, "󰀀"),
-	new constructNewCharacter("HA",  1, "󰀁"),
-	new constructNewCharacter("HE",  2, "󰀂"),
-	new constructNewCharacter("HI",  3, "󰀃"),
-	new constructNewCharacter("BO",  4, "󰀄"),
-	new constructNewCharacter("BA",  5, "󰀅"),
-	new constructNewCharacter("BE",  6, "󰀆"),
-	new constructNewCharacter("BI",  7, "󰀇"),
-	new constructNewCharacter("KO",  8, "󰀈"),
-	new constructNewCharacter("KA",  9, "󰀉"),
-	new constructNewCharacter("KE", 10, "󰀊"),
-	new constructNewCharacter("KI", 11, "󰀋"),
-	new constructNewCharacter("DO", 12, "󰀌"),
-	new constructNewCharacter("DA", 13, "󰀍"),
-	new constructNewCharacter("DE", 14, "󰀎"),
-	new constructNewCharacter("DI", 15, "󰀏"),
+	[ 'litteral' => 'HO',  'decimal' => ' 0',  'character' => '󰀀' ],
+	[ 'litteral' => 'HA',  'decimal' => ' 1',  'character' => '󰀁' ],
+	[ 'litteral' => 'HE',  'decimal' => ' 2',  'character' => '󰀂' ],
+	[ 'litteral' => 'HI',  'decimal' => ' 3',  'character' => '󰀃' ],
+	[ 'litteral' => 'BO',  'decimal' => ' 4',  'character' => '󰀄' ],
+	[ 'litteral' => 'BA',  'decimal' => ' 5',  'character' => '󰀅' ],
+	[ 'litteral' => 'BE',  'decimal' => ' 6',  'character' => '󰀆' ],
+	[ 'litteral' => 'BI',  'decimal' => ' 7',  'character' => '󰀇' ],
+	[ 'litteral' => 'KO',  'decimal' => ' 8',  'character' => '󰀈' ],
+	[ 'litteral' => 'KA',  'decimal' => ' 9',  'character' => '󰀉' ],
+	[ 'litteral' => 'KE',  'decimal' => '10',  'character' => '󰀊' ],
+	[ 'litteral' => 'KI',  'decimal' => '11',  'character' => '󰀋' ],
+	[ 'litteral' => 'DO',  'decimal' => '12',  'character' => '󰀌' ],
+	[ 'litteral' => 'DA',  'decimal' => '13',  'character' => '󰀍' ],
+	[ 'litteral' => 'DE',  'decimal' => '14',  'character' => '󰀎' ],
+	[ 'litteral' => 'DI',  'decimal' => '15',  'character' => '󰀏' ]
 ];
 
 function hex2bibi($givenHexadecimalNumber)
 {
-	for ($i = 0; $i <= 15; $i++) {
-		$j=dechex($i);
-		//str_replace($j,$decimal2bibinaryCorrespondence[$i],$hexadecimalNumber);
-		str_replace($j,$litt2dec2bibi["character"][$i],$givenHexadecimalNumber);
+	$bibinariesedNumber=strtolower($givenHexadecimalNumber);
+
+	for ($currentDecDigit = 0; $currentDecDigit <= 15; $currentDecDigit++) {
+		$currentHexDigit=dechex($currentDecDigit);
+		$currentBibiChar=$GLOBALS['litt2dec2bibi'][$currentDecDigit]['character'];
+
+		$bibinariesedNumber=str_replace($currentHexDigit, $currentBibiChar ,$bibinariesedNumber);
 	}
 
-	return $givenHexadecimalNumber;
+
+	return $bibinariesedNumber;
 }
 
 
